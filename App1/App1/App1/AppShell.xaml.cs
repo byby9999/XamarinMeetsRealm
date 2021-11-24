21a3ac09d5f7e7942d7fc4d7bd8a9672a672943f
alexandra-savescu <byby9999@users.noreply.github.com>
2021-11-24 17:22:06 +0200

    Initial files

---
+++ App1/App1/App1/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using App1.Views;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(Surgeries), typeof(Surgeries));
+        }
+
+    }
+}
