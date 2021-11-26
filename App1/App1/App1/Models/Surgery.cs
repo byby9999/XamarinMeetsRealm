2a13ddd19ab2791005f4be6b408eac6fa578f6c5
alexandra-savescu <byby9999@users.noreply.github.com>
2021-11-26 19:05:47 +0200

    Update models by schemas used

---
+++ App1/App1/App1/Models/Surgery.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using Realms;
+
+namespace App1.Models
+{
+    public class Surgery : RealmObject
+    {
+        [PrimaryKey]
+        [MapTo("_id")]
+        public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+        [MapTo("_partition")]
+        [Required]
+        public string Partition { get; set; }
+
+        public string SurgeryId { get; set; }
+
+        public string Reference { get; set; }
+        public string StartedOn { get; set; }
+
+        [Required]
+        public string ProcedureCode { get; set; }
+        [Required]
+        public string ProcedureName { get; set; }
+        public string BodySide { get; set; }
+        public string Theatre { get; set; }
+        public string SurgeonWithCode { get; set; }
+        public string PatientIdentificationNumber { get; set; }
+        public string Duration { get; set; }
+        public string Lines { get; set; }
+        public string SurgeryItemTotalCost { get; set; }
+        public string SurgeryItemTotalCostIncludingVat { get; set; }
+        public string SurgeryStaffTotalCost { get; set; }
+        public string TheatreTotalCost { get; set; }
+    }
+}
