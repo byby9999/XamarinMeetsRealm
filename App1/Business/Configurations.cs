c0fa2b83a8c5c2df4cd76acca41603b18efe6b1a
alexandra-savescu <alexandrabalean92@gmail.com>
2022-02-18 13:05:15 +0200

    Config update

--- App1/Business/Configurations.cs
+++ App1/Business/Configurations.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace App1.Business
 {
     public static class Configurations
     {
-        public const string  ChesterAccount_DefaultOrg_Project0_MyApp = "";
+        public const string  ChesterAccount_ChesterOrg_Project0_MyApp = "";
 
         public static Dictionary<string, string> UserPartitions = new Dictionary<string, string>()
         {
@@ -15,6 +16,8 @@ namespace App1.Business
             { "61c084f8f89724893240b892", "test56@example.com" },
             { "61c08508c0a82d01340e0458", "test78@example.com" }
         };
+
+        public static string DefaultPartition = UserPartitions.Keys.ElementAt(0);
 
         public static Dictionary<string, int> UsersDataVersionsMap = new Dictionary<string, int>()
         {
@@ -30,6 +33,6 @@ namespace App1.Business
         /// Important - No intermediary steps will be done. e.g. if a client is in version 1 and MaxVersionToUpgrade is set to 3,
         /// and client chooses "Update", they will update directly from 1 to 3, skipping version 2.
         /// This way, we can ensure clients only update to latest versions available.
-        public const int MaxVersionToUpgrade = 2;
+        public const int MaxVersionToUpgrade = 3;
     }
 }
